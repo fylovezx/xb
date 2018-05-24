cf235d82a124eac26738ed3ecc417190ad5e6066
fylovezx <490122386@qq.com>
2018-05-24 22:42:58 +0800

    delete useless

--- conn/conn.php
+++ conn/conn.php
@@ -1,6 +1,5 @@
 <?php
 $conn = new mysqli("106.15.90.17","cjcx","1234","lovezx");
-#$conn = new mysqli("127.0.0.1","cjcx","1234","lovezx");
 echo mysqli_connect_error();
 $conn->query("set names utf8");
 ?>
